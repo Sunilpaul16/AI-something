2ee12d7465165f122a5010061722a73ea43f102e
sunil paul <sunilpaul16@gmail.com>
2026-03-15 02:13:58 +0000

    Form

--- tauri-app/src/App.jsx
+++ tauri-app/src/App.jsx
@@ -1,50 +1,38 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <dev>
+      <h1>Hello</h1>
+      <Form />
+    </dev>
+  );
+}
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+function Form() {
+  const [text, setText] = useState("");
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
+  function handleClick(e) {
+    e.preventDefault();
+    console.log({ text });
+  }
+  return (
+    <>
+      <form method="post" onSubmit={handleClick}></form>
+      <label>
+        Text input:
         <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          type="text"
+          placeholder="Enter your message here"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+      </label>
+      <button className="ask" type="submit" onClick={handleClick}>
+        Submit
+      </button>
+    </>
   );
 }
 
