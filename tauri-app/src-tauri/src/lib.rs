8bc4517a2fa4faf1221d9cebda831aaccd489cb7
sunil paul <sunilpaul16@gmail.com>
2026-03-28 04:25:17 +0000

    improve error

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -11,5 +11,5 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error running tauri application");
 }
